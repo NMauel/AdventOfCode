92fb452a4007cfd306d96e5f060c62d28c681316
Niels Mauel <niels@rebelit.nl>
2021-12-07 09:09:06 +0100

    Added solutions Day 7.

---
+++ AdventConsole/2021/Day7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventCode
+{
+    public class Day7 : IPuzzleDay<int>
+    {
+        private IEnumerable<int> input = InputReader.ReadString("Day7.txt").Split(',').Select(int.Parse);
+
+        public int CalculateAnswerPuzzle1()
+        {
+            var leastPossibleFuel = int.MaxValue;
+
+            for (int position = input.Min(); position <= input.Max(); position++)
+                leastPossibleFuel = Math.Min(leastPossibleFuel, input.Sum(crab => Math.Abs(position - crab)));
+
+            return leastPossibleFuel;
+        }
+
+        public int CalculateAnswerPuzzle2()
+        {
+            var leastPossibleFuel = int.MaxValue;
+
+            for (int position = input.Min(); position <= input.Max(); position++)
+                leastPossibleFuel = Math.Min(leastPossibleFuel, input.Sum(crab => (Math.Abs(position - crab) * (Math.Abs(position - crab) + 1)) / 2));
+
+            return leastPossibleFuel;
+        }
+    }
+}

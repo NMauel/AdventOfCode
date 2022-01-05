1013214126b103a813feb66e4dd693ab832b7bd0
Niels Mauel <niels@rebelit.nl>
2022-01-05 21:50:21 +0100

    Updated Day1 solutions.

--- AdventConsole/2021/Day1.cs
+++ AdventConsole/2021/Day1.cs
@@ -7,14 +7,8 @@ namespace AdventCode
     {
         private readonly IEnumerable<int> input = InputReader.ReadLines("Day1.txt").Select(int.Parse);
 
-        public int CalculateAnswerPuzzle1()
-        {
-            return input.Pairwise((a, b) => a < b).Count(r => r);
-        }
+        public int CalculateAnswerPuzzle1() => input.Pairwise((a, b) => a < b).Count(r => r);
 
-        public int CalculateAnswerPuzzle2()
-        {
-            return input.Triowise((a, b, c) => a + b + c).Pairwise((a, b) => a < b).Count(r => r);
-        }
+        public int CalculateAnswerPuzzle2() => input.Triowise((a, b, c) => a + b + c).Pairwise((a, b) => a < b).Count(r => r);
     }
 }

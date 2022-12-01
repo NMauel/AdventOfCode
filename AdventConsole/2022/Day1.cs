92d45c2a6772d158cbedaf8c8d68a3556d96fe96
Niels Mauel <nmauel@vmi-group.com>
2022-12-01 11:37:20 +0100

    Added Day1 solutions of 2022

---
+++ AdventConsole/2022/Day1.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventCode.Aoc2022;
+
+public class Day1 : IPuzzleDay
+{
+    private readonly IEnumerable<string> calories = InputReader.ReadLines();
+
+    public object CalculateAnswerPuzzle1() => SumCaloriesPerElve().Max();
+
+    public object CalculateAnswerPuzzle2() => SumCaloriesPerElve().OrderDescending().Take(3).Sum();
+
+    private IEnumerable<int> SumCaloriesPerElve()
+    {
+        var index = 0;
+        while(index < calories.Count())
+            yield return calories.Skip(index).TakeWhile(c => { index++; return !string.IsNullOrWhiteSpace(c); }).Select(int.Parse).Sum(); 
+    }
+}

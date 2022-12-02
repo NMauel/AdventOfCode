537a1c2b8df2f46080e4627a2e2ad34d38f0c67d
Niels Mauel <niels@rebelit.nl>
2022-12-02 06:58:07 +0100

    Added solution day 2

---
+++ AdventConsole/2022/Day2.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventCode.Aoc2022;
+
+public class Day2 : IPuzzleDay
+{
+    private readonly IEnumerable<string> input = InputReader.ReadLines();
+
+    private readonly int[] matchpoints = new[] { 6, 0, 3 };
+    public object CalculateAnswerPuzzle1() => input.Select(x => matchpoints[(x[2] - x[0]) % 3] + x[2] - 87).Sum();
+
+    private readonly int[] handpoints = new[] { 3, 1, 2 };
+    public object CalculateAnswerPuzzle2() => input.Select(x => ((x[2] - 88) * 3) + handpoints[(x[0] + x[2]) % 3]).Sum();
+}

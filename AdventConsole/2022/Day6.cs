e2397be102d62db3910e91936b7585c43edb7e19
Niels Mauel <niels@rebelit.nl>
2022-12-11 21:28:38 +0100

    Added solutions Day 6

---
+++ AdventConsole/2022/Day6.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventCode.Aoc2022;
+
+public class Day6 : IPuzzleDay
+{
+    private readonly string message = InputReader.ReadText();
+
+    public object CalculateAnswerPuzzle1() => FindFirstOccurrenceOfDistinctChars(message, 4);
+
+    public object CalculateAnswerPuzzle2() => FindFirstOccurrenceOfDistinctChars(message, 14);
+
+    private static int FindFirstOccurrenceOfDistinctChars(string datastream, int length)
+    {
+        for (var i = 0; i < datastream.Count() - length; i++)
+            if (datastream.Skip(i).Take(length).Distinct().Count() == length)
+                return i + length;
+        return 0;
+    }
+}

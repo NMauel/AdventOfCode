7415eadb9bc0f7b1dc1e60047b01f9518bef6608
Niels Mauel <niels@rebelit.nl>
2022-12-20 07:18:22 +0100

    Added solutions Day 20

---
+++ AdventConsole/2022/Day20.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventCode.Aoc2022;
+
+public class Day20 : IPuzzleDay
+{
+    public object CalculateAnswerPuzzle1()
+    {
+        var magic = new MagicList(InputReader.ReadLines().Select(i => (Guid.NewGuid(), long.Parse(i))));
+        var itemsToMix = new Queue<(Guid Id, long Value)>(magic);
+
+        return MixAndCalc(magic, itemsToMix);
+    }
+
+    public object CalculateAnswerPuzzle2()
+    {
+        var magic = new MagicList(InputReader.ReadLines().Select(i => (Guid.NewGuid(), long.Parse(i) * 811589153)));
+        var itemsToMix = new Queue<(Guid Id, long Value)>(Enumerable.Repeat(magic, 10).SelectMany(m => m));
+
+        return MixAndCalc(magic, itemsToMix);
+    }
+
+    private long MixAndCalc(MagicList magic, Queue<(Guid Id, long Value)> itemsToMix)
+    {
+        magic.Mix(itemsToMix);
+
+        var zeroIndex = magic.IndexOf(magic.Single(x => x.Value == 0));
+
+        return magic[zeroIndex + 1000] + magic[zeroIndex + 2000] + magic[zeroIndex + 3000];
+    }
+
+    public class MagicList : List<(Guid Id, long Value)>
+    {
+        public MagicList(IEnumerable<(Guid, long)> values) => this.AddRange(values);
+
+        public new long this[int index] => base[index % base.Count].Value;
+
+        public void Mix(Queue<(Guid Id, long Value)> itemsToMix)
+        {
+            while (itemsToMix.Any())
+            {
+                var item = itemsToMix.Dequeue();
+                var newIndex = (int)((base.IndexOf(item) + item.Value) % (this.Count - 1));
+
+                if (newIndex < 1)
+                    newIndex = this.Count - 1 + newIndex;
+
+                base.Remove(item);
+                base.Insert(newIndex, item);
+            }
+        }
+
+        public override string ToString() => string.Join(',', this.Select(x => x.Value));
+    }
+}
